9e033341fcda8b4252dd6cbd03eb5b899e92d82c
Will McGinty <mcgintw@gmail.com>
2018-09-15 00:02:17 -0400

    Derp

--- Sources/App/Controllers/PushController.swift
+++ Sources/App/Controllers/PushController.swift
@@ -12,14 +12,14 @@ struct PushController {
     
     func sendNewMessagePush(for message: Message, to recipients: [User.ID], with worker: Request) throws -> Future<HTTPStatus> {
         let urbanService = try worker.make(UrbanVaporService.self)
-        let client = try worker.client()
+        //let client = try worker.client()
         
         return User.findAll(recipients, on: worker).flatMap { users in
             return try users.map {
                 let notification = UrbanVapor.Notification(alert: message.contents)
                 let push = Push(audience: Audience(namedUser: $0.email), notification: notification, deviceTypes: DeviceTypes(deviceTypes: .ios))
                 
-                return try urbanService.send(push, on: client).map { response in
+                return try urbanService.send(push, on: worker).map { response in
                     let logger = try worker.make(Logger.self)
                     logger.info(String(describing: response.http.status))
                     logger.info(String(describing: response.http.body))
